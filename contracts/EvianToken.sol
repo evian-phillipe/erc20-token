46a1ac95410f9ff611a67a90997d40a8f99f431b
evian-phillipe <info@lsscoltd.com>
2026-04-13 20:36:10 +0100

    Update EvianToken.sol

--- contracts/EvianToken.sol
+++ contracts/EvianToken.sol
@@ -1,1 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+
+contract EvianToken is ERC20, Ownable {
+    uint256 public immutable maxSupply;
+
+    constructor(uint256 _maxSupply) ERC20("EvianToken", "EVN") Ownable(msg.sender) {
+        maxSupply = _maxSupply * 10 ** decimals();
+        _mint(msg.sender, 1000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        require(totalSupply() + amount <= maxSupply, "Max supply exceeded");
+        _mint(to, amount);
+    }
+}
